c752ea3b96e54a657da65dd6f26ac94be372abd5
agxmeister <agxmeister@gmail.com>
2024-11-23 12:55:54 +0200

    Move the interaction with the Open AI to a class.

--- src/MagicBall.tsx
+++ src/MagicBall.tsx
@@ -8,96 +8,86 @@ import {
 import {Message} from "openai/resources/beta/threads/messages";
 import {Assistant} from "openai/resources/beta/assistants";
 
-export async function statusHandler(client: OpenAI, run: Run, threadId: string, actionHandler: Function)
+export default class MagicBall
 {
-    console.log(run.status);
-    if (run.status === "completed") {
-        let messages = await client.beta.threads.messages.list(threadId);
-        console.log(messages.data);
-        return messages.data;
-    } else if (
-        run.status === "requires_action" &&
-        run.required_action &&
-        run.required_action.submit_tool_outputs &&
-        run.required_action.submit_tool_outputs.tool_calls
-    ) {
-        return await statusHandler(
-            client,
-            await actionHandler(
-                client,
-                threadId,
-                run.id,
-                run.required_action.submit_tool_outputs.tool_calls,
-            ),
-            threadId,
-            actionHandler,
-        );
-    } else {
-        console.error("Run did not complete:", run);
+    readonly client: OpenAI;
+
+    constructor(client: OpenAI)
+    {
+        this.client = client;
     }
-}
 
-export async function actionHandler(
-    client: OpenAI,
-    threadId: string,
-    runId: string,
-    tools: Array<RequiredActionFunctionToolCall>
-): Promise<Run>
-{
-    return await client.beta.threads.runs.submitToolOutputsAndPoll(threadId, runId, {
-        tool_outputs: tools.map<RunSubmitToolOutputsAndPollParams.ToolOutput>(
-            (tool: { function: { name: string; }; id: any; }): RunSubmitToolOutputsAndPollParams.ToolOutput => {
-                if (tool.function.name === "publishWebsite") {
-                    console.log('Make the publication...')
-                    return {
-                        tool_call_id: tool.id,
-                        output: "Website was published!",
-                    };
-                } else {
-                    return {
-                        tool_call_id: tool.id,
-                        output: "",
-                    };
-                }
-            },
-        )
-    });
-}
+    async statusHandler(client: OpenAI, run: Run, threadId: string): Promise<any>
+    {
+        console.log(run.status);
+        if (run.status === "completed") {
+            let messages = await client.beta.threads.messages.list(threadId);
+            console.log(messages.data);
+            return messages.data;
+        } else if (
+            run.status === "requires_action" &&
+            run.required_action &&
+            run.required_action.submit_tool_outputs &&
+            run.required_action.submit_tool_outputs.tool_calls
+        ) {
+            return this.statusHandler(
+                client,
+                await this.actionHandler(
+                    client,
+                    threadId,
+                    run.id,
+                    run.required_action.submit_tool_outputs.tool_calls,
+                ),
+                threadId,
+            );
+        } else {
+            console.error("Run did not complete:", run);
+        }
+    }
 
-interface MagicBall {
-    askForIntention: Function,
-    createThread: Function,
-    addUserMessage: Function,
-    createAssistant: Function,
-    runConversation: Function,
-}
+    async actionHandler(
+        client: OpenAI,
+        threadId: string,
+        runId: string,
+        tools: Array<RequiredActionFunctionToolCall>
+    ): Promise<Run>
+    {
+        return await client.beta.threads.runs.submitToolOutputsAndPoll(threadId, runId, {
+            tool_outputs: tools.map<RunSubmitToolOutputsAndPollParams.ToolOutput>(
+                (tool: { function: { name: string; }; id: any; }): RunSubmitToolOutputsAndPollParams.ToolOutput => {
+                    if (tool.function.name === "publishWebsite") {
+                        console.log('Make the publication...')
+                        return {
+                            tool_call_id: tool.id,
+                            output: "Website was published!",
+                        };
+                    } else {
+                        return {
+                            tool_call_id: tool.id,
+                            output: "",
+                        };
+                    }
+                },
+            )
+        });
+    }
 
-export default function getMagicBall(apiKey: string): MagicBall {
-    const client = new OpenAI({
-        apiKey: apiKey,
-    });
-    return {
-        askForIntention: async (input: string) => await client.chat.completions.create({
-         messages: [{ role: 'user', content: `
-            Classify the input text into one of these topics:
-            - Add a completely new article
-            - Expand an existing article with new thoughts
-            Put only a topic name in the output.
-            
-            Input: "${input}"
-            Output:`
-         }],
-         model: 'gpt-4o-mini',
-        }),
+    async createThread(): Promise<Thread>
+    {
+        return this.client.beta.threads.create();
+    }
 
-        createThread: async (): Promise<Thread> => await client.beta.threads.create(),
-
-        addUserMessage: async (threadId: string, userInput: string): Promise<Message> => client.beta.threads.messages.create(threadId, {
+    async addUserMessage(threadId: string, userInput: string): Promise<Message>
+    {
+        return this.client.beta.threads.messages.create(threadId, {
             role: "user",
             content: userInput,
-        }),
+        });
+    }
 
-        createAssistant: async (): Promise<Assistant> => await client.beta.assistants.create({
+    async createAssistant(): Promise<Assistant>
+    {
+        return this.client.beta.assistants.create({
             model: "gpt-4o",
             instructions:
                 "You are assisting with changing the content on the user's website. Use the provided functions to help the user.",
@@ -111,13 +101,14 @@ export default function getMagicBall(apiKey: string): MagicBall {
                     },
                 },
             ],
-        }),
+        });
+    }
 
-        runConversation: async (threadId: string, assistantId: string, statusHandler: Function, actionHandler: Function) => {
-            const run = await client.beta.threads.runs.createAndPoll(threadId, {
-                assistant_id: assistantId,
-            });
-            return await statusHandler(client, run, threadId, actionHandler);
-        },
-    };
+    async runConversation(threadId: string, assistantId: string)
+    {
+        const run = await this.client.beta.threads.runs.createAndPoll(threadId, {
+            assistant_id: assistantId,
+        });
+        return this.statusHandler(this.client, run, threadId);
+    }
 }
--- src/app/api/assistances/route.ts
+++ src/app/api/assistances/route.ts
@@ -1,20 +1,17 @@
-import getMagicBall from "@/MagicBall";
-import {statusHandler, actionHandler} from "@/MagicBall";
+import MagicBall from "@/MagicBall";
+import OpenAI from "openai";
 
 export async function POST(request: Request) {
     const data = await request.json();
 
-    const magicBall = getMagicBall(process.env.OPENAI_API_KEY ?? '');
+    const magicBall = new MagicBall(new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+    }));
 
     const thread = await magicBall.createThread();
     await magicBall.addUserMessage(thread.id, data.input);
     const assistant = await magicBall.createAssistant();
-    const output = await magicBall.runConversation(
-        thread.id,
-        assistant.id,
-        statusHandler,
-        actionHandler,
-    );
+    const output = await magicBall.runConversation(thread.id, assistant.id);
 
     return Response.json(output);
 }
