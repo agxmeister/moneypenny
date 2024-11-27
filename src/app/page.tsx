ea19fa41790c6b5a83438aa31c39ffed6cbcae15
agxmeister <agxmeister@gmail.com>
2024-11-27 18:07:13 +0200

    Clean up the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import styles from "./page.module.css"
 
 export default function Home() {
     return (
-        <>Hello, <span className={styles.bold}>world</span>! Meanwhile, the Open API key is {process.env.OPENAI_API_KEY}</>
+        <div>Hello, <span className={styles.bold}>world</span>!</div>
     );
 }
