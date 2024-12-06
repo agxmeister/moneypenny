13117217f88735288a2522868826a8fa605fd857
agxmeister <agxmeister@gmail.com>
2024-12-06 16:04:57 +0200

    Add the ability to remove all the interactions at once.

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -1,8 +1,9 @@
 import MagicBall from "@/MagicBall";
 import OpenAI from "openai";
-import {writeFileSync} from "node:fs";
+import {writeFileSync, readdirSync, unlinkSync} from "node:fs";
 
-export async function POST() {
+export async function POST()
+{
     const magicBall = new MagicBall(new OpenAI({
         apiKey: process.env.OPENAI_API_KEY
     }));
@@ -11,4 +12,27 @@ export async function POST() {
     writeFileSync("./interaction.json", JSON.stringify(thread));
 
     return Response.json(thread);
+}
+
+export async function DELETE()
+{
+    const magicBall = new MagicBall(new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+    }));
+
+    for (const file of readdirSync("./insights")) {
+        const interactionId = file.match(/(?<interactionId>.*).json/i)?.groups?.interactionId
+        if (!interactionId) {
+            continue;
+        }
+        try {
+            await magicBall.removeThread(interactionId);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            unlinkSync(`./insights/${file}`);
+        }
+    }
+
+    return Response.json([]);
 }
